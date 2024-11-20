7fae108573e3124075c76446c5c4cf7993fd437c
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-11-20 19:38:08 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -254,9 +254,9 @@ async def doc(bot, update):
         await message.reply("**Error:** Target channel not set. Use /set_target to set the channel.")
         return
 
-    ms = await bot.update.message.send_message(chat_id=TARGET_CHANNEL_ID, text=data + "🚀 Start Downloading From the Website ⚡")
+    ms = await client.send_message(chat_id=TARGET_CHANNEL_ID, text=data + "🚀 Start Downloading From the Website ⚡")
     try:
-     	path = await bot.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram,progress_args=(data, "🚀  Downloading Anime From the Website ⚡", ms, time.time()))                    
+     	path = await client.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram,progress_args=(data, "🚀  Downloading Anime From the Website ⚡", ms, time.time()))                    
     except Exception as e:
      	return await ms.edit(e)
     
